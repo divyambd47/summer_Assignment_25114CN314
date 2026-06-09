380b60cf37d90dd0f849ea802c67d5842b299f4c
divyambd47 <divyambd47@gmail.com>
2026-06-08 21:40:12 -0700

    Upload Day_13 programs

---
+++ Day_13/program4.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],i,n;
+    int even=0,odd=0;
+
+    printf("Enter size of array:");
+    scanf("%d",&n);
+
+    printf("Enter %d elements:\n",n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+
+        if(arr[i] % 2 == 0)
+        {
+            even++;
+        }
+        else
+        {
+            odd++;
+        }
+    }
+
+    printf("Even Elements = %d\n",even);
+    printf("Odd Elements =%d\n",odd);
+
+    return 0;
+}
